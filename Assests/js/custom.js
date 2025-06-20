0e9336237551aac5e1085bd98f49ee7c6745d734
IJ910 <ia6956994@gmail.com>
2025-06-20 09:00:02 +0545

    initial update

---
+++ Assests/js/custom.js
@@ -0,0 +1,11 @@
+$('.owl-carousel').owlCarousel({
+    loop: true,
+    center: true,
+    margin: 10,
+    items: 3.5,
+    nav: true,
+    autoplay: true,
+    autoplayTimeout: 1500,
+    autoplayHoverPause: true,
+
+})
